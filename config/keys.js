af5b9eb85e62bc3b1b8b63144959fa76f148cfed
Zaytt <ivan_chavez_e@hotmail.com>
2019-03-22 13:47:43 -0600

    Fixed Keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://zaytt:password1@ds049754.mlab.com:49754/zaytt-storybooks-dev',
-  googleClientID: '87517563967-hd5mbqeimtb2hk2qt1d1pasksv01uskt.apps.googleusercontent.com',
-  googleClientSecret: 'fFnTUY81Xv4ZnN0niD9Wiz8A'
+if(process.env.NODE_ENV == 'production' ){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
